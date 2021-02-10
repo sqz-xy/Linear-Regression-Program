1c1acc420d154e020d30c550c6a8561f1add570c
Tom Beet <tblakebeet@gmail.com>
2021-02-10 22:02:17 +0000

    Completely overhauled the code

--- Computational_Assessment.py
+++ Computational_Assessment.py
@@ -3,41 +3,71 @@
 
 ########################################################################################################################################################################################################
 
-#Reads the lines in the file
-lines_in_file = open("Coercivity_Mag_Data.dat", 'r').readlines()
-number_of_lines = len(lines_in_file)
+#FIX ERRORS OUT THE ASS 
+#DISPLAU REGRESSION ON GRAPH
 
-string_without_line_breaks = ""
+import math as mth
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
-a_file = open("Coercivity_Mag_Data.dat", 'r')
+class program:
 
-#Goes through each line in the file and removes the /n
-for line in a_file:
+  def displayGraph(self):
+    # Data for plotting
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2 * np.pi * t)
 
-  stripped_line = line.rstrip()
+    fig, ax = plt.subplots()
+    ax.plot(t, s)
 
-  string_without_line_breaks += stripped_line
+    ax.set(xlabel='beans time (s)', ylabel='bean (mV)',
+       title='beans density, beans')
+    ax.grid()
 
-a_file.close()
+    fig.savefig("test.png")
+    plt.show()
+    
+  def regression(self, fCoerc, fMag): #Regression Method
 
-#Prints the string without "/n"
-print(string_without_line_breaks)
-#Splits the string into an array
-NewData = string_without_line_breaks.split(":")
-#Removes the last empty value which appeared for some reason
-NewData.pop()
+    X = fCoerc
+    y = fMag
 
-#Declares 2 arrays for Coercivity and Magnetisation
-Coercivity = []
-Magnetisation = []
+    model = LinearRegression()
+    model.fit(X, y)
 
-Coercivity = NewData[::2] #Starts at the first element and does every other element after
-Magnetisation = NewData[1::2] #Starts at the second element, then every other
+    X_predict = fMag
+    y_precit = model.predict(X_predict)
 
-print(Coercivity)
-print(Magnetisation)
+  def readFile(self): #File reading method
 
-#Regression time
+    string_without_line_breaks = ""
+    a_file = open("Coercivity_Mag_Data.dat", 'r')
+
+    for line in a_file: #Goes through each line in the file and removes the /n
+
+      stripped_line = line.rstrip()
+
+      string_without_line_breaks += stripped_line
+
+    a_file.close() #Closes the file
+    NewData = string_without_line_breaks.split(":") #Splits the string into an array
+    NewData.pop() #Removes the last empty value which appeared for some reason
+    Coercivity = [] #Declares 2 arrays for Coercivity and Magnetisation
+    Magnetisation = []
+    Coercivity = NewData[::2] #Starts at the first element and does every other element after
+    Magnetisation = NewData[1::2] #Starts at the second element, then every other
+
+    self.regression(Coercivity, Magnetisation) #Calls the regression method
+
+newProgram = program()
+newProgram.displayGraph()
+newProgram.readFile()
+    
+
+
+
+
 
 
 
